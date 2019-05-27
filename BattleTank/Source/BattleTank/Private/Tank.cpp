0ff31f5134ba1b66672fcc75ad1c92a89a47f7c3
Sergei Belokon <sergei.e.belokon@gmail.com>
2019-05-27 18:10:50 +0300

    Added instantiation of projectiles

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -4,6 +4,8 @@
 #include "TankAimingComponent.h"
 #include "TankBarrel.h"
 #include "TankTurret.h"
+#include "Projectile.h"
+
 
 // Sets default values
 ATank::ATank()
@@ -22,11 +24,20 @@ void ATank::Fire()
 {
     auto Time = GetWorld()->GetTimeSeconds();
     UE_LOG(LogTemp, Warning, TEXT("Fire in the hole %f"), Time);
+    if (!Barrel) { return; }
+    UE_LOG(LogTemp, Warning, TEXT("MUST WORK %f"), Time);
+    // Spawn a projectile at the socket location on the barrel
+    GetWorld()->SpawnActor<AProjectile>(
+        ProjectileBlueprint,
+        Barrel->GetSocketLocation(FName("Projectile")),
+        Barrel->GetSocketRotation(FName("Projectile"))
+    );
 }
 
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
     TankAimingComponent->SetBarrelReference(BarrelToSet);
+    Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -10,6 +10,7 @@
 class UTankAimingComponent;
 class UTankBarrel;
 class UTankTurret;
+class AProjectile;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -45,4 +46,10 @@ public:
 
     UPROPERTY(EditAnywhere, Category = Firing)
     float LaunchSpeed = 8000;
+    
+    UPROPERTY(EditAnywhere, Category = Setup)
+    TSubclassOf<AProjectile> ProjectileBlueprint;
+    
+    // Local barrel reference for spawning projectile
+    UTankBarrel *Barrel = nullptr;
 };
